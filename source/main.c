ac15bfcdde4d3de4bf92dde06c8ebba9f9143de7
lolinatorishere <78425011+Lolinatorishere@users.noreply.github.com>
2025-04-15 16:12:59 +0100

    Created a menu printing function Created Menu.h header to allow c to compile the source files created Menu.c in source to create the implementation of the code in c created menuPrint() that prints out the menu text equivilant of the input
     Changes to be committed:
    	new file:   headers/menu.h
    	new file:   menus/menuPrint0.menu
    	modified:   source/main.c
    	new file:   source/menu.c

--- source/main.c
+++ source/main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../headers/userinput.h"
+#include "../headers/stringParse.h"
+#include "../headers/menu.h"
 
 int main(){
-    char *test = (char*)malloc(sizeof(char)*25);
-    strcpy(test,"this is a test 111111111");
-    textInputSanitize(test, "1", '\0');
-    printf("Hello World\n");
-    printf("%li\n", int64NumberInput("7448954290"));
-    printf("%.5f\n",doubleNumberInput("2385342.838749"));
-    printf("%s", test);
-    free(test);
+    char test[] = "hello world lmao 11111";
+    //char filter[1] = "o";
+    //char replace[1] = "a";
+    //stringReplace(test, "o", "a");
+    menuPrint(0, 1, 3);
 }
---
+++ source/menu.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "../headers/stringParse.h"
+#ifdef _WIN32
+    #define syscls system("cls")
+#endif
+#ifdef __linux__
+    #define syscls system("clear")
+#endif
+#define TXT_CONST 56
+#define INDENT 4
+#define MARGIN 4
+
+//prints a line of "space" aka " "
+void dynamic_linespace(int text_constant, int txt_indent, int txt_margin){
+    char space[1] = " ";
+    printf("|%*c|\n", text_constant + txt_indent + txt_margin, space[0]);
+}
+
+void dynamic_begin_text(int text_constant, int txt_indent, int txt_margin){
+    char line[1] = "-";
+    printf("/");
+    for(int i = 0; i < text_constant + txt_indent + txt_margin; i++){
+        printf("%c",line[0]);
+    }
+    printf("\\\n");
+}
+
+void dynamic_end_text(int text_constant, int txt_indent, int txt_margin){
+    char line[1] = "-";
+    printf("\\");
+    for(int i = 0; i < text_constant + txt_indent + txt_margin; i++){
+        printf("%c",line[0]);
+    }
+    printf("/\n");
+}
+
+void dynamic_line_print(char *string, int text_const, int txt_indent, int txt_margin){
+    //have to check for \n chars and replace with pretty much a dymanic line space
+    //recieves a string that is read and then it is printed in a certain way
+    int arraysize = strlen(string);
+    if(arraysize == 0) return;
+    char textconst = text_const;
+    if(text_const == 0) textconst = 1;
+    char space[1] = " ";
+    char buffer[256] = {'\0'};
+    int i = 0, j = 0;
+    printf("|%*c", txt_indent, space[0]);
+    for(i = 0 ; i <= arraysize; i++){
+        if(string[i] == '\n' && i+1 != arraysize){
+            printf("%*c|\n", textconst-j + txt_margin, space[0]);
+            j = 0;
+            printf("|%*c", txt_indent, space[0]);
+            continue;
+        }
+        if(textconst <= j){
+            printf("%*c|\n", textconst-j + txt_margin, space[0]);
+            j = 0;
+            printf("|%*c", txt_indent, space[0]);
+            continue;
+        }
+        if(i+1 == arraysize){
+            printf("%*c|\n", textconst-j + txt_margin, space[0]);
+            continue;
+        }
+        printf("%c",string[i]);
+        j++;
+    }
+}
+
+int menuPrint(int menuSection, int padding_top, int padding_bottom){
+    syscls;
+    char buffer[256] = {'\0'};
+    char *dir = malloc(sizeof(char)*256);
+    int64_t filesize = 0;
+    strcpy(dir, "./menus/menuPrint");
+    snprintf(buffer, 256, "%i", menuSection);
+    strcat(dir,buffer);
+    strcat(dir,".menu");
+    FILE *fp = fopen(dir, "r");
+    if(fp == NULL) return -1;
+    fseek(fp, 0, SEEK_END);
+    filesize = ftell(fp);
+    if(filesize == 0) return -1;
+    fseek(fp, 0, SEEK_SET);
+    char *menuText = (char*)malloc(sizeof(char)*filesize);
+    for(int i = 0 ; i < filesize ; i++){
+        fseek(fp, i, SEEK_SET);
+        menuText[i] = fgetc(fp);
+    }
+    dynamic_begin_text(TXT_CONST, INDENT, MARGIN);
+    for(int i = 0 ; i < padding_top; i++){
+        dynamic_linespace(TXT_CONST, INDENT, MARGIN);
+    }
+    dynamic_line_print(menuText, TXT_CONST, INDENT, MARGIN);
+    for(int i = 0 ; i < padding_bottom; i++){
+        dynamic_linespace(TXT_CONST, INDENT, MARGIN);
+    }
+    dynamic_end_text(TXT_CONST, INDENT, MARGIN);
+    free(menuText);
+    free(dir);
+    fclose(fp);
+}
+
